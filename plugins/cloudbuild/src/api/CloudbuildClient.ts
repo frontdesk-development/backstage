4a02bffaeb4a32401c277443ab1acc75ed02e858
ebarrios <esteban.barrios@trivago.com>
2020-10-22 17:37:11 +0200

    Test for waiting for triggers part7

--- plugins/cloudbuild/src/api/CloudbuildClient.ts
+++ plugins/cloudbuild/src/api/CloudbuildClient.ts
@@ -54,7 +54,9 @@ export class CloudbuildClient implements CloudbuildApi {
     const workflowRuns = await fetch(
       `https://cloudbuild.googleapis.com/v1/projects/${encodeURIComponent(
         projectId,
-      )}/builds?trigger_id=(\"${encodeURIComponent(triggerId || '')}\")`,
+      )}/builds?filter=(trigger_id=(\"${encodeURIComponent(
+        triggerId || '',
+      )}\"))`,
       {
         headers: new Headers({
           Accept: '*/*',
